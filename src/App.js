6adc2fd5875a17488122360987f18679fda9617c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-23 02:40:35 +0600

    react coding functional

--- src/App.js
+++ src/App.js
@@ -14,24 +14,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name="Rubel" naiyaka = "Moushumi"></Person>
+        <Person name="Manna" naiyaka = "Moushumi"></Person>
+        <Person name="Eliash K" naiyaka = "Champa"></Person>
+        <Person name="Arefin" naiyaka = "pori"></Person>
       </header>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   // const personStyle = {
   //   border: '2px solid red',
   //   margin: '10px'
   // } 
+  console.log(props);
   return (
     <div style = {{border:'2px solid red', margin:'5px', padding:'5px'}}>
-      <h1>Name: Shakib al Hasan</h1>
-      <h3>Title: Hero</h3>
+      <h1>Name: {props.name}</h1>
+      <h3>Title: Hero of {props.naiyaka}</h3>
     </div>
   )
 

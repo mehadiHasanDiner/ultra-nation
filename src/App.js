ca589cacde236e93390482becfd046845348760e
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-23 01:35:36 +0600

    react coding

--- src/App.js
+++ src/App.js
@@ -4,34 +4,37 @@ import reactDom from 'react-dom';
 
 var style = {
   color: "red",
-  backgroundColor:"yellow"
+  backgroundColor: "yellow"
 }
 
 function App() {
-  var person1 ={
-    name: "আমার নাম মেহেদী",
-    age: "আমার বয়স ৩২",
-    salary: 500
-  }
-  var person2 ={
-    name: "আমার নাম লিমা",
-    age: "আমার বয়স 20",
-    salary: 100
-  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>My heading</h1>
-        <p>My first React Paragraph</p>
-        <h4 className="" style={style}>{person1.name +". "+ person1.age + ". Salary: " + person1.salary}</h4>
-        <p style ={{color:'green', background:'yellow', fontWeight:'bold'}}>{person2.name +". "+ person2.age + ". Salary: " + person2.salary}</p>
+        <p>I am a react person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person() {
+  // const personStyle = {
+  //   border: '2px solid red',
+  //   margin: '10px'
+  // } 
+  return (
+    <div style = {{border:'2px solid red', margin:'5px', padding:'5px'}}>
+      <h1>Name: Shakib al Hasan</h1>
+      <h3>Title: Hero</h3>
+    </div>
+  )
+
 }
 
 export default App;

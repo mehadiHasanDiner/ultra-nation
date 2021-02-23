8c0f901f6fc9bbd75d3bfeddb82c7bb09338f143
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-23 23:03:05 +0600

    video-10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
+import { useState } from 'react';
 
 var style = {
   color: "red",
@@ -10,61 +11,70 @@ var style = {
 function App() {
 
   const products = [
-    {name:'Photoshop', price: '$90.99'},
-    {name:'Illustrator', price: '$60.99'},
-    {name:'PDF Reader', price: '$6.99'},
-    {name:'Adobe Primier', price: '$250.99'}
+    { name: 'Photoshop', price: '$90.99' },
+    { name: 'Illustrator', price: '$60.99' },
+    { name: 'PDF Reader', price: '$6.99' },
+    { name: 'Adobe Primier', price: '$250.99' }
   ]
-const nayoks = ['Razzak', 'Bappi', 'Bappi', 'Shuvo', 'Sakib', 'Omor'];
-
-  const productName = products.map(product => product.name);
-  console.log(productName);
+  const nayoks = ['Razzak', 'Bappi', 'Bappi', 'Shuvo', 'Sakib', 'Omor'];
 
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
-        {/* <Product name ={products[0].name} Price={products[0].Price}></Product>         */}
-        <ul>{
-              nayoks.map(nayok => <li>{nayok}</li>)
-              // nayoks.map(nayok => <li></li>)
-          }
+        <Count></Count>
 
-          {
-              products.map(product=> <li>{product.name}</li>)
-          }
-
-        
-            {/* <li>{nayoks[0].toUpperCase()}</li>
-            <li>{nayoks[1].toUpperCase()}</li>
-            <li>{nayoks[2].toUpperCase()}</li>
-            <li>{nayoks[3].toUpperCase()}</li> */}
-        </ul>
-
-         {
-            products.map(product=> <Product product={product}></Product>)
-          }
-        {/* <Product product = {products[0]}></Product>        
-        <Product product = {products[1]}></Product>         */}
       </header>
     </div>
   );
 }
 
+function Count() {
+  const [count, setCount] = useState(10);
+  const handleIncrease = () => setCount(count + 1);
+
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onMouseMove={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+
+
+
+// function Count() {
+//   const [count, setCount] = useState(10);
+//   const handleIncrease = () => {
+//     const newCount = count + 1;
+//     setCount(newCount);
+//   };
+  
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={handleIncrease}>Increase</button>
+//     </div>
+//   )
+// }
+
 function Product(props) {
   const productStyle = {
     border: '1px solid gray',
     borderRadius: '5px',
-    backgroundColor:'lightpink',
+    backgroundColor: 'lightpink',
     height: '220px',
-    width:'250px',
+    width: '250px',
     float: 'left'
   }
   // const {name, price} = props.product;
   // const {name, price} = {name:'Photoshop', Price: '$90.99'};
-  const {name, price} = props.product;
+  const { name, price } = props.product;
   console.log(props);
-  return(
+  return (
     <div style={productStyle}>
       <h3>Name: {name}</h3>
       <h5> Price: {price}</h5>
